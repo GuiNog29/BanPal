56878625f37d6703ebd445faf542253bef3f4d6f
Guilherme Almeida <guimathiasgui@gmail.com>
2023-05-17 22:03:28 -0300

    Retirando interface para implementar melhor posteriormente

--- src/Infra/Repository/User/Interface/IUserRepository.ts
+++
@@ -1,9 +0,0 @@
-import { Account } from "../../../../Domain/Account/Entity/Account";
-import { User } from "../../../../Domain/User/Entity/User";
-
-export interface IUserRepository {
-  createUser(user: User): Promise<User>
-  getAllUsers(): Promise<User & { account: Account | null; }>
-  updateUser(user: User): Promise<User>
-  deleteUser(id: string): Promise<User>
-}
