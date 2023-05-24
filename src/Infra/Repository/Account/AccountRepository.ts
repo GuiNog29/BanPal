0181c98b0128fe26655dccfd93aea54199e3c535
Guilherme Almeida <guilherme.almeida@benner.com.br>
2023-05-24 19:32:03 -0300

    Revert "Desenvolvimento - 23/05/2023"

--- src/Infra/Repository/Account/AccountRepository.ts
+++ src/Infra/Repository/Account/AccountRepository.ts
@@ -1,7 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
-const depositOperation = 1
 
 export class AccountRepository {
 
@@ -15,63 +14,4 @@ export class AccountRepository {
 
     return bankAccount;
   }
-
-  async getBalance(userId: number){
-    const balance = await prisma.account.findFirst({
-      where: {
-        userId: userId
-      }
-    });
-
-    return balance?.balance;
-  }
-
-  async deposit(userId: number, valueDeposit: number){
-    const newBalance = await this.OperationBank(userId, valueDeposit, depositOperation)
-
-    return newBalance;
-  }
-
-  async getAccount(userId: number){
-    let currentAccount = await prisma.account.findFirst({
-      where: {
-        userId: userId
-      }
-    });
-
-    return currentAccount;
-  }
-
-  async OperationBank(userId: number, valueDeposit: number, depositOperation: number) {
-
-    const currentAccount = await this.getAccount(userId);
-
-    switch (depositOperation) {
-      case 1:
-        currentAccount!.balance += valueDeposit;
-        break;
-
-      default:
-        break;
-    }
-
-    const updatedBalance = this.updateBalance(currentAccount!.id, currentAccount!.balance);
-
-    return updatedBalance;
-  }
-
-
-  async updateBalance(accountId: number, newBalance: number) {
-    return await prisma.account.update({
-      where: {
-        id: accountId
-      },
-      data: {
-        balance: newBalance,
-      }
-    });
-  }
 }
-
-
-
